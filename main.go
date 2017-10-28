96e1ee5526afbb051a69c8fcdb51c6cd4afb6117
Osuka42g <osuka42@gmail.com>
2017-10-28 17:15:04 +0700

    Add basic systray

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/getlantern/systray"
+)
+
+func main() {
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	systray.SetIcon(getIcon("assets/clock.ico"))
+	systray.SetTitle("I'm alive!")
+	systray.SetTooltip("Look at me, I'm a tooltip!")
+}
+
+func onExit() {
+
+}
+
+func getIcon(s string) []byte {
+	b, err := ioutil.ReadFile(s)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return b
+}
