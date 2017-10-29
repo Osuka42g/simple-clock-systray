aaae52b4454b04521bdfed5243aeecaea207dde9
Osuka42g <osuka42@gmail.com>
2017-10-29 12:54:16 +0700

    Add timezone selector

--- main.go
+++ main.go
@@ -9,26 +9,65 @@ import (
 	"github.com/getlantern/systray"
 )
 
+var (
+	timezone string
+)
+
 func main() {
 	systray.Run(onReady, onExit)
 }
 
 func onReady() {
+	timezone = "Local"
+
 	systray.SetIcon(getIcon("assets/clock.ico"))
-	for {
-		systray.SetTitle(getTime())
-		systray.SetTooltip("Look at me, I'm a tooltip!")
-		time.Sleep(1 * time.Second)
-	}
+
+	localTime := systray.AddMenuItem("Local time", "Local time")
+	hcmcTime := systray.AddMenuItem("Ho Chi Minh time", "Asia/Ho_Chi_Minh")
+	sydTime := systray.AddMenuItem("Sydney time", "Australia/Sydney")
+	gdlTime := systray.AddMenuItem("Guadalajara time", "America/Mexico_City")
+	sfTime := systray.AddMenuItem("San Fransisco time", "America/Los_Angeles")
+	systray.AddSeparator()
+	mQuit := systray.AddMenuItem("Quit", "Quits this app")
+
+	go func() {
+		for {
+			systray.SetTitle(getClockTime(timezone))
+			systray.SetTooltip(timezone + " timezone")
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-localTime.ClickedCh:
+				timezone = "Local"
+			case <-hcmcTime.ClickedCh:
+				timezone = "Asia/Ho_Chi_Minh"
+			case <-sydTime.ClickedCh:
+				timezone = "Australia/Sydney"
+			case <-gdlTime.ClickedCh:
+				timezone = "America/Mexico_City"
+			case <-sfTime.ClickedCh:
+				timezone = "America/Los_Angeles"
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+				return
+			}
+		}
+	}()
 }
 
 func onExit() {
 	// Cleaning stuff here.
 }
 
-func getTime() string {
+func getClockTime(tz string) string {
 	t := time.Now()
-	hour, min, sec := t.Clock()
+	utc, _ := time.LoadLocation(tz)
+
+	hour, min, sec := t.In(utc).Clock()
 	return ItoaTwoDigits(hour) + ":" + ItoaTwoDigits(min) + ":" + ItoaTwoDigits(sec)
 }
 
