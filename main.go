75de47a1bfd5d29c4e961aae64e878e50b4ec8e1
Osuka42g <osuka42@gmail.com>
2017-10-29 11:09:59 +0700

    Refresh systray title each second with time

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"time"
 
 	"github.com/getlantern/systray"
 )
@@ -13,12 +15,27 @@ func main() {
 
 func onReady() {
 	systray.SetIcon(getIcon("assets/clock.ico"))
-	systray.SetTitle("I'm alive!")
-	systray.SetTooltip("Look at me, I'm a tooltip!")
+	for {
+		systray.SetTitle(getTime())
+		systray.SetTooltip("Look at me, I'm a tooltip!")
+		time.Sleep(1 * time.Second)
+	}
 }
 
 func onExit() {
 	// Cleaning stuff here.
+}
+
+func getTime() string {
+	t := time.Now()
+	hour, min, sec := t.Clock()
+	return ItoaTwoDigits(hour) + ":" + ItoaTwoDigits(min) + ":" + ItoaTwoDigits(sec)
+}
+
+// ItoaTwoDigits time.Clock returns one digit on values, so we make sure to convert to two digits
+func ItoaTwoDigits(i int) string {
+	b := "0" + strconv.Itoa(i)
+	return b[len(b)-2:]
 }
 
 func getIcon(s string) []byte {
